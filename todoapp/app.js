447d0e787e7f8fc7413dadceed221e4c8cbd96eb
charvi1 <125857988+charvi1@users.noreply.github.com>
2024-10-22 15:22:48 +0530

    Update app.js

--- todoapp/app.js
+++ todoapp/app.js
@@ -3,16 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// Middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Set EJS as the view engine
+
 app.set('view engine', 'ejs');
 
-// Static files for styles
-app.use(express.static('public'));
 
-// Array to hold tasks
+
 let tasks = [];
 
 // GET /todo - Display the form and task list
@@ -32,11 +29,11 @@ app.post('/todo', (req, res) => {
 // POST /delete - Delete a task from the list
 app.post('/delete', (req, res) => {
     const taskIndex = req.body.taskIndex;
-    tasks.splice(taskIndex, 1); // Remove the task by index
+    tasks.splice(taskIndex, 1);
     res.redirect('/todo');
 });
 
-// Start the server
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
